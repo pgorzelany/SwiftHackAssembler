de2c2e17c44e78cf3f73430a1f95bf6454253a62
pgorzelany <piotr.gorzelany@gmail.com>
2018-04-10 20:55:41 +0200

    added a core framework dependency

--- Package.swift
+++ Package.swift
@@ -12,8 +12,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SwiftHackAssembler",
-            dependencies: []),
+        .target(name: "SwiftHackAssembler", dependencies: ["SwiftHackAssemblerCore"]),
+        .target(name: "SwiftHackAssemblerCore", dependencies: [])
     ]
 )
